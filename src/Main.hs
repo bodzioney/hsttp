ad93fb3a61a22dd3e444e8b1b5c25549961198b3
ethanbodzioney <etbodz@gmail.com>
2024-04-11 19:43:48 -0400

    Added some security headers

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,13 @@
 module Main where
 
-import Network.Socket
-import System.IO 
-import System.Directory
 import Control.Concurrent
-import Data.List.Split
 import qualified Data.ByteString as B
-import qualified Data.ByteString.UTF8 as BSU 
+import qualified Data.ByteString.UTF8 as BSU
+import Data.List.Split
 import Magic
+import Network.Socket
+import System.Directory
+import System.IO
 
 main :: IO ()
 main = do
@@ -26,26 +26,35 @@ sockHandler sock = do
   forkIO $ handleConn handle
   sockHandler sock
 
+headers :: BSU.ByteString
+headers =
+  BSU.fromString
+    "Content-Security-Policy: frame-ancestors 'none'\r\n\
+    \X-Frame-Options: DENY\r\n\
+    \X-Content-Type-Options: nosniff\r\n\
+    \Referrer-Policy: same-origin\r\n"
+
 handleConn :: Handle -> IO ()
 handleConn handle = do
   request <- hGetLine handle
-  let (_: uri: _) = splitOn " " (head (lines request))
+  let (_ : uri : _) = splitOn " " (head (lines request))
   fileExists <- doesFileExist (tail uri)
   if fileExists
-  then do 
-    magic <- magicOpen [MagicMime]
-    magicLoadDefault magic
-    mime <- magicFile magic (tail uri)
-    contents <- B.readFile (tail uri) 
-    B.hPut handle $ BSU.fromString "HTTP/1.1 200 OK\r\n"
-    B.hPut handle $ BSU.fromString ("Content-Type: " ++ mime ++ "\r\n")
-    B.hPut handle $ BSU.fromString "\r\n"
-    B.hPut handle contents 
-  else do 
-    B.hPut handle $ BSU.fromString "HTTP/1.1 404 Not Found\r\n"
-    B.hPut handle $ BSU.fromString "Content-Type: text/html\r\n"
-    B.hPut handle $ BSU.fromString "\r\n"
-    B.hPut handle $ BSU.fromString "<h1>404 Not Found</h1>"
+    then do
+      magic <- magicOpen [MagicMime]
+      magicLoadDefault magic
+      mime <- magicFile magic (tail uri)
+      contents <- B.readFile (tail uri)
+      B.hPut handle $ BSU.fromString "HTTP/1.1 200 OK\r\n"
+      B.hPut handle headers
+      B.hPut handle $ BSU.fromString ("Content-Type: " ++ mime ++ "\r\n")
+      B.hPut handle $ BSU.fromString "\r\n"
+      B.hPut handle contents
+    else do
+      B.hPut handle $ BSU.fromString "HTTP/1.1 404 Not Found\r\n"
+      B.hPut handle $ BSU.fromString "Content-Type: text/html\r\n"
+      B.hPut handle headers
+      B.hPut handle $ BSU.fromString "\r\n"
+      B.hPut handle $ BSU.fromString "<h1>404 Not Found</h1>"
 
   hClose handle
-
